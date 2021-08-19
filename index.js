5d2778a9b2b7f181b9bf418cbcbe650a95ae7721
Gia Jennings <gjennings.co@gmail.com>
2021-08-19 19:17:29 -0400

    commit tree b7eda5fdcbc8553f4340b596b1916b695a1c45c7

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2; 
+
+const random = Math.random(1) + 1;
+
+const num3 = 100
+const num4 = 25
+const mod = num3 % num4;
+
+const max = Math.max(1,2,3,20)
